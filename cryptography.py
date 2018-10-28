d025380c4cdc56ab511adac4b1c7adb1623548f3
ca9800 <42943809+ca9800@users.noreply.github.com>
2018-10-27 22:47:29 -0400

    Updated from Brython Server: 10/27/2018 10:47:29 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Claire
+Credit: 
 
 Assignment:
 
@@ -10,35 +10,113 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations=list(associations)
 
 
+def encryption(x):
+    enkey = input("Key: ")
+    
+    #turns input message to numbers
+    x = list(x)
+    a = 0
+    b = []
+    
+    for m in x:
+        (x.append(associations.index(m)))
+    
+    
+    #turns key messsage to numbers
+    c = []
+    for m in enkey:
+        (c.append(associations.index(m)))
+    
+    #adds numbers together
+    d = []
+    
+    while a < len (b):
+        d.append((b[a]+c[a%len(d)])%len(associations))
+        x=x+1
+    
+    #turns numbers back to letters
+    x = 0
+    e = []
+    
+    while x<len(c):
+        (e.append(associations[c[x]]))
+        x=x+1
+    
+    
+    #turns to a list 
+    k = ""
+    for m in e:
+        k=k+m
+    print (k)
+    return (k)
+    
+    
+def decryption(e):
+    enkey = input("Key: ")
+    
+    #turns input message to numbers
+    x = list(x)
+    a = 0
+    b = []
+    #enkey = (str(input("Key: ")))
+    #e = list(c)
+    #c = []
+    
+    for m in x:
+        (x.append(associations.index(m)))
+    
+    
+    #turns key messsage to numbers
+    c = []
+    for m in enkey:
+        c.append(associations.index(m))
+    
+    #adds numbers together
+    d = []
+    
+    while a < len (b):
+        d.append((b[a]+c[a%len(d)])%len(associations))
+        x=x+1
+    
+    #turns numbers back to letters
+    x = 0
+    e = []
+    
+    while x<len(c):
+        e.append(associations[c[x]])
+        x=x+1
+    
+    
+    #turns to a list 
+    k = ""
+    for m in e:
+        k=k+m
+    print (k)
+    return (k)
+    
 
-
-
-
-typ = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-message = input("Message: ")
-key = input("Key: ")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def requesting_ans():
+    req=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if req=="e":
+        mess=str(input("Message: "))
+        encryption(mess)
+        requesting_ans()
+    elif req=="d":
+        mess=str(input("Message: "))
+        decryption(mess)
+        requesting_ans()
+    elif req=="q":
+        print("Goodbye!")
+        return()
+    else:
+        print("Did not understand command, try again.")
+        requesting_ans()
+        
+requesting_ans()
+    
+    
+    
+        
